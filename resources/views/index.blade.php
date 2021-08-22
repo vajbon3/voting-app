8c171ab53e488dc638c0571e5b1c29748eac9c6d
vajbon3 <notvaja@gmail.com>
2021-08-22 15:02:01 +0400

    commit 2 - Design Idea Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,12 +17,79 @@
             </select>
         </div>
         <div class="w-2/3 relative">
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl placeholder-gray-900 bg-white px-4 py-2 pl-8 border-none">
+            <input type="search" placeholder="Find an idea"
+                   class="w-full rounded-xl placeholder-gray-900 bg-white px-4 py-2 pl-8 border-none">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
                 </svg>
             </div>
         </div>
     </div> <!-- end filters -->
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container flex hover:shadow-card cursor-pointer rounded-xl bg-white transition duration-150 ease-in">
+            <div class="idea-container bg-white rounded-xl flex">
+                <div class="border-r border-gray-100 px-5 py-8">
+                    <div class="text-center">
+                        <div class="font-semibold text-2xl">12</div>
+                        <div class="text-gray-500">Votes</div>
+                    </div>
+
+                    <div class="mt-8">
+                        <button
+                            class="w-20 bg-gray-200 border border-gray-200 font-bold hover:border-gray-400 transition duration-150 ease-in text-xxs uppercase rounded-xl px-4 py-3">
+                            Vote
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
+                         class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="" class="hover:underline">random title</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At doloribus dolorum ea eius expedita, ipsa magni nam officia provident quasi quisquam sed similique sit vel voluptatum. Laboriosam, nulla, rem. Consequatur dolore eaque esse repudiandae. Amet aut, autem commodi cumque debitis enim et fuga incidunt iusto laboriosam magni molestiae molestias nam nihil odio officia perspiciatis porro praesentium quas saepe sed similique sint. Accusantium alias dicta, dolor doloremque eos in laborum numquam omnis, rem sequi sit suscipit tempora veniam! A asperiores beatae cum cumque debitis distinctio dolorum eligendi est ex, facilis harum id ipsam laudantium molestiae nulla, quod quos rerum? Sit, ullam!
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div
+                                class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                                Open
+                            </div>
+                            <button
+                                class="bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                                <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#"
+                                           class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark
+                                            as Spam</a></li>
+                                    <li><a href="#"
+                                           class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete
+                                            Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end ideas-container -->
+    </div> <!-- end ideas-container -->
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,13 +28,21 @@ module.exports = {
             maxWidth: {
                 "custom": "62.5rem",
             },
+            boxShadow: {
+                card: "4px 4px 15px 0 rgba(36, 37, 38, 0.08)",
+                dialog: "3px 4px 15px 0 rgba(36, 37, 38, 0.22)",
+            },
             spacing: {
+                44:  "11rem",
                 70: "17.5rem",
                 175: "43.75rem",
             },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                xxs: ["0.625rem", { lineHeight: "1rem"}],
+            }
         },
     },
 
@@ -44,5 +52,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("@tailwindcss/line-clamp")
+    ],
 };
